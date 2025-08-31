7c6afea1d0073e3b9553b1c96066f2e314272911
Anyul Rivas <anyulled@gmail.com>
2025-08-31 14:24:00 +0200

    feat: add unreliable endpoint to HelloController for testing purposes

--- service-provider/src/main/java/com/example/provider/web/HelloController.java
+++ service-provider/src/main/java/com/example/provider/web/HelloController.java
@@ -1,9 +1,13 @@
 package com.example.provider.web;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 @RestController
 @RequestMapping("/api")
@@ -15,5 +19,16 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         return "Hola desde " + appName;
+    }
+
+    // New endpoint that fails approximately 70% of the time with HTTP 500
+    @GetMapping("/unreliable")
+    public ResponseEntity<String> unreliable() {
+        int rnd = ThreadLocalRandom.current().nextInt(100);
+        if (rnd < 70) { // fail 70% of requests
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Service failure (simulated). Random=" + rnd);
+        }
+        return ResponseEntity.ok("Success from " + appName + " (Random=" + rnd + ")");
     }
 }
